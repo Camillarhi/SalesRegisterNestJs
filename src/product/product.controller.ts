8bc20cf1700808d2b86023479623733b77ec3c7d
Camillarhi <rhitanene@gmail.com>
2022-08-02 17:29:51 +0100

    init

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,15 +1,16 @@
 import { Controller, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { UserService } from 'src/user/user.service';
+import { ProductService } from './product.service';
 
 
 @ApiTags("Product")
 @UseGuards(JwtAuthGuard)
 @Controller('product')
 export class ProductController {
-    // constructor(
-    //     private customerInvoiceService: CustomerinvoiceService,
-    //     private dailyRecordService: DailyrecordService,
-    //     private userService: UserService
-    // ) { }
+    constructor(
+        private productService: ProductService,
+        private userService: UserService
+    ) { }
 }
